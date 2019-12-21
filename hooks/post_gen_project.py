d8f8014abc954339d5281759dc392842e5004f7a
Raffaele Colace <raffaele@20tab.com>
2019-12-21 12:54:21 +0100

    init project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,29 +3,8 @@ import os
 import shutil
 
 
-def remove(filepath):
-    if os.path.isfile(filepath):
-        os.remove(filepath)
-    elif os.path.isdir(filepath):
-        shutil.rmtree(filepath)
+def create_apps():
+    os.system('./bin/init.sh')
 
 
-# def copy_secrets():
-    
-#     with open("k8s/secrets.yaml.tpl", "r") as f:
-#         text = f.read()
-#         text_development = text.replace("__ENVIRONMENT__", "development")
-#         text_integration = text.replace("__ENVIRONMENT__", "integration")
-#         text_production = text.replace("__ENVIRONMENT__", "production")
-
-#         with open("k8s/development/secrets.yaml", "w+") as fd:
-#             fd.write(text_development)
-
-#         with open("k8s/integration/secrets.yaml", "w+") as fd:
-#             fd.write(text_integration)
-        
-#         with open("k8s/production/secrets.yaml", "w+") as fd:
-#             fd.write(text_production)
-
-
-# copy_secrets()
+create_apps()
