8071a504cf579c87d43254cd8758b0e7eb499e26
Paolo Melchiorre <paolo@20tab.com>
2020-02-19 14:19:53 +0100

    Fix project python files formats

--- {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/settings.py
@@ -84,7 +84,7 @@ class ProjectDefault(Configuration):
     DATABASES = values.DatabaseURLValue()
 
     # Password validation
-    # https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators  # noqa
+    # https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
     # fmt: off
     AUTH_PASSWORD_VALIDATORS = [
         {
@@ -139,7 +139,9 @@ class ProjectDefault(Configuration):
         (("{{cookiecutter.project_slug}}", "errors@{{cookiecutter.project_slug}}.com"),)
     )
 
-    DEFAULT_FROM_EMAIL = "{{cookiecutter.project_slug}} <info@{{cookiecutter.project_slug}}.com>"
+    DEFAULT_FROM_EMAIL = (
+        "{{cookiecutter.project_slug}} <info@{{cookiecutter.project_slug}}.com>"
+    )
 
     EMAIL_SUBJECT_PREFIX = "[{{cookiecutter.project_slug}}] "
 
--- {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/wsgi.py
+++ {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/wsgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_slug}}.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_slug}}.settings"
+)
 os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
 from configurations.wsgi import get_wsgi_application  # noqa isort:skip
