5af63205a388a3f9ea1042b4e821b4d0d869dab8
Raffaele Colace <raffaele@20tab.com>
2020-03-27 16:50:29 +0100

    media volumes moved to backend service

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 """Define hooks to be run after project generation."""
 
+import os
 import subprocess
 
 
@@ -7,5 +8,14 @@ def init():
     """Execute intialization script."""
     subprocess.run("./scripts/init.sh")
 
+def set_media_volumes():
+    use_media_volume = "{{ cookiecutter.use_media_volume }}" == "Yes"
+
+    if not use_media_volume:
+        os.remove("k8s/development/1_volumes.yaml")
+        os.remove("k8s/integration/1_volumes.yaml")
+        os.remove("k8s/production/1_volumes.yaml")
+
 
 init()
+set_media_volumes()
