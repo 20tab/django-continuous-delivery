9ff0176e4b2a885b4566d65a520f0e9a77b623e8
Paolo Melchiorre <paolo@20tab.com>
2020-05-05 12:55:26 +0200

    🎨️ Improve post gen project hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 """Define hooks to be run after project generation."""
 
+import sys
 from pathlib import Path
 from shutil import copyfile
 
 
+def create_env_file():
+    """Create env file from the template."""
+    env_file = Path(".env")
+    env_template = Path(".env.tpl")
+    try:
+        copyfile(env_template, env_file)
+    except FileNotFoundError:
+        sys.exit(f"File {env_template} not found.")
+
+
+def remove_media_volumes():
+    """Remove volumes files in all kubernetes sub-directories."""
+    for path in Path("k8s").glob("*/1_volumes.yaml"):
+        try:
+            path.unlink()
+        except FileNotFoundError:
+            sys.exit(f"File '{path}' not found.")
+
+
 def main():
-    """Execute intialization steps."""
-    copyfile(Path(".env.tpl"), Path(".env"))
+    """Execute intialization steps after project generation."""
+    create_env_file()
     if "{{cookiecutter.use_media_volume}}" == "No":
-        for f in Path("k8s").glob("*/1_volumes.yaml"):
-            try:
-                f.unlink()
-            except FileNotFoundError:
-                pass
+        remove_media_volumes()
 
 
 if __name__ == "__main__":
