d6d9404d587cda3df29a8bfeaf622a5158741e15
Paolo Melchiorre <paolo@melchiorre.org>
2021-03-01 18:35:49 +0100

    Remove broken bash strict mode for scripts

--- {{cookiecutter.project_dirname}}/scripts/behave.sh
+++ {{cookiecutter.project_dirname}}/scripts/behave.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# Bash "strict mode", to help catch problems and bugs in the shell script
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-
 python3 manage.py behave --configuration=Testing --simple
--- {{cookiecutter.project_dirname}}/scripts/check.sh
+++ {{cookiecutter.project_dirname}}/scripts/check.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Bash "strict mode", to help catch problems and bugs in the shell script
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-
 black -q --check .
 isort -q --check .
 flake8
