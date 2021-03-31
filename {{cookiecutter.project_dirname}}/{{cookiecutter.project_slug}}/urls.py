77956ac4b962ddf2a6b55fc22d776bc0219836a4
Niccolò Mineo <niccolo.mineo@gmail.com>
2021-03-31 16:37:48 +0200

    Fix urls.py

--- {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
@@ -24,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG:  # pragma: no cover
     urlpatterns += [
         re_path(
             r"^media/(?P<path>.*)$",
