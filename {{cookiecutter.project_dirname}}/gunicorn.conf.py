d403f1b43b709808387ef03de1a69b4af24478fa
Paolo Melchiorre <paolo@melchiorre.org>
2021-04-12 09:18:32 +0200

    Fix #147 Improve gunicorn configuration file

--- {{cookiecutter.project_dirname}}/gunicorn.conf.py
+++ {{cookiecutter.project_dirname}}/gunicorn.conf.py
@@ -1,4 +1,19 @@
 """Gunicorn configuration file."""
 
+# Logging
+# https://docs.gunicorn.org/en/stable/settings.html#logging
+
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+# Server Socket
+# https://docs.gunicorn.org/en/stable/settings.html#server-socket
+
 bind = "0.0.0.0:8000"
+
+# Worker Processes
+# https://docs.gunicorn.org/en/stable/settings.html#worker-processes
+
 worker_class = "uvicorn.workers.UvicornWorker"
