523e3ca2e74acf07563015679f0aee0540f7b3d2
daniele <daniele.pompa@20tab.com>
2021-10-22 08:58:49 +0200

    Update from siblings project

---
+++ bootstrap.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+"""Boostrap a web project based on templates."""
+
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+import click
+from cookiecutter.main import cookiecutter
+from slugify import slugify
+
+GITLAB_TOKEN_ENV_VAR = "GITLAB_PRIVATE_TOKEN"
+OUTPUT_BASE_DIR = os.getenv("OUTPUT_BASE_DIR")
+
+
+def create_env_file(service_dir):
+    """Create env file from the template."""
+    env_path = Path(service_dir) / Path(".env_template")
+    env_template = env_path.read_text()
+    env_path.write_text(env_template)
+
+
+def init_service(
+    service_dir,
+    project_dirname,
+    project_name,
+    project_slug,
+    service_slug,
+    output_dir,
+):
+    """Initialize the service."""
+    if Path(service_dir).is_dir() and click.confirm(
+        f'A directory "{service_dir}" already exists and ' "must be deleted. Continue?"
+    ):
+        shutil.rmtree(service_dir)
+    """Initialize the backend service project."""
+    cookiecutter(
+        ".",
+        extra_context={
+            "project_dirname": project_dirname,
+            "project_name": project_name,
+            "project_slug": project_slug,
+            "service_slug": service_slug,
+        },
+        output_dir=output_dir,
+        no_input=True,
+    )
+
+
+def init_gitlab(
+    gitlab_group_slug,
+    gitlab_private_token,
+    service_dir,
+    project_name,
+    project_slug,
+):
+    """Initialize the Gitlab repositories."""
+    env = {
+        "TF_VAR_gitlab_group_slug": gitlab_group_slug,
+        "TF_VAR_gitlab_token": gitlab_private_token,
+        "TF_VAR_service_dir": service_dir,
+        "TF_VAR_project_name": project_name,
+        "TF_VAR_project_slug": project_slug,
+    }
+    subprocess.run(
+        ["terraform", "init", "-reconfigure", "-input=false"],
+        cwd="terraform",
+        env=env,
+    )
+    subprocess.run(
+        [
+            "terraform",
+            "apply",
+            "-auto-approve",
+            "-input=false",
+        ],
+        cwd="terraform",
+        env=env,
+    )
+
+
+def change_output_owner(service_dir, user_id):
+    """Change the owner of the output directory recursively."""
+    user_id is not None and subprocess.run(f"chown -R {user_id} {service_dir}")
+
+
+def slugify_option(ctx, param, value):
+    """Slugify an option value."""
+    return value and slugify(value)
+
+
+@click.command()
+@click.option("--output-dir", default=".", required=OUTPUT_BASE_DIR is None)
+@click.option("--project-name", prompt=True)
+@click.option("--project-slug", callback=slugify_option)
+@click.option("--service-slug", callback=slugify_option)
+@click.option("--project-dirname")
+@click.option("--use-gitlab/--no-gitlab", is_flag=True, default=None)
+@click.option("--gitlab-private-token", envvar=GITLAB_TOKEN_ENV_VAR)
+@click.option("--gitlab-group-slug")
+def init_handler(
+    output_dir,
+    project_name,
+    project_slug,
+    service_slug,
+    project_dirname,
+    use_gitlab,
+    gitlab_private_token,
+    gitlab_group_slug,
+):
+    """Init the bootstrap handler."""
+    project_slug = slugify(
+        project_slug or click.prompt("Project slug", default=slugify(project_name)),
+    )
+    service_slug = slugify(
+        service_slug or click.prompt("Service slug", default="django"),
+    )
+    project_dirname = project_dirname or click.prompt(
+        "Project dirname",
+        default=service_slug,
+        type=click.Choice([service_slug, project_slug]),
+    )
+    output_dir = OUTPUT_BASE_DIR or output_dir
+    service_dir = (Path(output_dir) / Path(project_dirname)).resolve()
+    init_service(
+        service_dir,
+        project_dirname,
+        project_name,
+        project_slug,
+        service_slug,
+        output_dir,
+    )
+    create_env_file(service_dir)
+    use_gitlab = (
+        use_gitlab
+        if use_gitlab is not None
+        else click.confirm("Do you want to configure Gitlab?", default=True)
+    )
+    if use_gitlab:
+        gitlab_group_slug = gitlab_group_slug or click.prompt(
+            "Gitlab group slug", default=project_slug
+        )
+        click.confirm(
+            f'Make sure the Gitlab "{gitlab_group_slug}" group exists '
+            "before proceeding. Continue?",
+            abort=True,
+        )
+        gitlab_private_token = gitlab_private_token or click.prompt(
+            "Gitlab private token (with API scope enabled)", hide_input=True
+        )
+        init_gitlab(
+            gitlab_group_slug,
+            gitlab_private_token,
+            service_dir,
+            project_name,
+            project_slug,
+        )
+
+
+if __name__ == "__main__":
+    init_handler()
---
+++ terraform/main.tf
@@ -0,0 +1,109 @@
+locals {
+  user_data = jsondecode(data.http.user_info.body)
+
+  git_config = "-c user.email=${local.user_data.email} -c user.name=\"${local.user_data.name}\""
+}
+
+terraform {
+  backend "local" {
+  }
+
+  required_providers {
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "3.7.0"
+    }
+  }
+}
+
+provider "gitlab" {
+  token = var.gitlab_token
+}
+
+data "gitlab_group" "group" {
+  full_path = var.gitlab_group_slug
+}
+
+data "http" "user_info" {
+  url = "https://gitlab.com/api/v4/user"
+
+  request_headers = {
+    Accept        = "application/json"
+    Authorization = "Bearer ${var.gitlab_token}"
+  }
+}
+
+resource "gitlab_project" "backend" {
+  name                   = "Backend"
+  path                   = "backend"
+  description            = "The \"${var.project_name}\" project backend service."
+  namespace_id           = data.gitlab_group.group.id
+  initialize_with_readme = false
+}
+
+resource "null_resource" "init_backend" {
+  depends_on = [gitlab_branch_protection.develop_backend]
+
+  triggers = {
+    backend_project_id = gitlab_project.backend.id
+  }
+
+  provisioner "local-exec" {
+    command = join(" && ", [
+      "cd ${var.service_dir}",
+      format(
+        join(" && ", [
+          "git init --initial-branch=develop",
+          "git remote add origin %s",
+          "git add .",
+          "git ${local.git_config} commit -m 'Initial commit'",
+          "git push -u origin develop -o ci.skip",
+          "git checkout -b master",
+          "git push -u origin master -o ci.skip",
+          "git remote set-url origin %s",
+        ]),
+        replace(
+          gitlab_project.backend.http_url_to_repo,
+          "/^https://(.*)$/",
+          "https://oauth2:${var.gitlab_token}@$1"
+        ),
+        gitlab_project.backend.ssh_url_to_repo,
+
+      )
+    ])
+  }
+}
+
+resource "gitlab_branch_protection" "develop_backend" {
+  project            = gitlab_project.backend.id
+  branch             = "develop"
+  push_access_level  = "maintainer"
+  merge_access_level = "developer"
+}
+
+resource "gitlab_branch_protection" "master_backend" {
+  depends_on = [null_resource.init_backend]
+
+  project            = gitlab_project.backend.id
+  branch             = "master"
+  push_access_level  = "no one"
+  merge_access_level = "maintainer"
+}
+
+resource "gitlab_tag_protection" "tags_backend" {
+  project             = gitlab_project.backend.id
+  tag                 = "*"
+  create_access_level = "maintainer"
+}
+
+resource "gitlab_project_badge" "coverage_backend" {
+  project   = gitlab_project.backend.id
+  link_url  = "https://${var.project_slug}.gitlab.io/backend/"
+  image_url = "https://gitlab.com/%%{project_path}/badges/%%{default_branch}/pipeline.svg"
+}
+
+resource "gitlab_project_badge" "pipeline_backend" {
+  project   = gitlab_project.backend.id
+  link_url  = "https://gitlab.com/%%{project_path}/pipelines"
+  image_url = "https://gitlab.com/%%{project_path}/badges/%%{default_branch}/pipeline.svg"
+}
