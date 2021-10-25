5e637782e6157210611dbeb64ad379f7183f83d5
Filippo Morelli <filippo@20tab.com>
2021-10-25 19:54:15 +0000

    Fix aborting when project dir exists

--- bootstrap.py
+++ bootstrap.py
@@ -361,7 +361,8 @@ def init_command(
         warning(
             f'A directory "{service_dir}" already exists and '
             "must be deleted. Continue?",
-        )
+        ),
+        abort=True
     ):
         shutil.rmtree(service_dir)
     project_url_dev = project_url_dev or click.prompt(
