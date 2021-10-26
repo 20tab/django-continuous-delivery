8b55757b0f6b3fac5a85eaf27c83cf1f194363ef
Matteo Vitali <trottomv@gmail.com>
2021-10-26 08:47:19 +0200

    Fix verify pacts test

---
+++ {{cookiecutter.project_dirname}}/tests/verify_pacts.py
@@ -0,0 +1,58 @@
+"""Define tests to verify pacts."""
+
+import re
+
+from pactman.verifier.verify import ProviderStateMissing
+
+
+class ProviderStatesHandler:
+    """A handler for provider states."""
+
+    def __init__(self):
+        """Initialize the instance."""
+        self.handlers = []
+        self.context = {}
+
+    def register(self, state_matcher):
+        """Register the given function as a handler."""
+
+        def outer_wrapper(function):
+            """Register and return the function."""
+            try:
+                pattern = re.compile(state_matcher, re.IGNORECASE)
+            except re.error:
+                raise ValueError(f"Invalid pattern provided: {state_matcher}.")
+            self.handlers.append((pattern, function))
+            return function
+
+        return outer_wrapper
+
+    def handle(self, state, context):
+        """Handle the given provider state."""
+        handlers = iter(self.handlers)
+        while True:
+            try:
+                pattern, function = next(handlers)
+            except StopIteration:
+                break
+            else:
+                result = pattern.match(state)
+                if result:
+                    return function(context=context, **result.groupdict())
+        raise ProviderStateMissing(state)
+
+    def tear_down(self):
+        """Clean up after handling states."""
+        try:
+            self.context["traveller"].stop()
+        except KeyError:
+            pass
+
+    def run(self, name, **params):
+        """Set up the given provider state."""
+        self.context = {**params}
+        for handler_name in name.split("/"):
+            self.handle(handler_name.strip(), self.context)
+
+
+handler = ProviderStatesHandler()
