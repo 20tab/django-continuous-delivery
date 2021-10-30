2393b2fb7576da81170bc9e16452f6cf7a279fb9
Matteo Vitali <trottomv@gmail.com>
2021-10-30 18:30:22 +0200

    Add safety for dependency vulnerabilities check

--- {{cookiecutter.project_dirname}}/scripts/check.sh
+++ {{cookiecutter.project_dirname}}/scripts/check.sh
@@ -6,5 +6,5 @@ python3 -m black --check .
 python3 -m isort --check .
 python3 -m flake8
 python3 -m mypy .
-python3 -m bandit -r -q -x tests .
-python3 -m jake ddt -q -r requirements/remote.txt
+python3 -m bandit -r -q -x /tests/ .
+python3 -m safety check --file requirements/remote.txt
