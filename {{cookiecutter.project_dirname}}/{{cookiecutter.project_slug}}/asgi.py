e07725a68c8e845ecd719209b7a404e874f04c89
Paolo Melchiorre <paolo@melchiorre.org>
2021-11-11 17:16:26 +0100

    Fix #183 Upgrade django-configrations

--- {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/asgi.py
+++ {{cookiecutter.project_dirname}}/{{cookiecutter.project_slug}}/asgi.py
@@ -9,14 +9,11 @@ https://docs.djangoproject.com/en/stable/howto/deployment/asgi/
 
 import os
 
-import configurations
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_slug}}.settings"
 )
 
-configurations.setup()
+from configurations.asgi import get_asgi_application  # noqa isort:skip
 
 application = get_asgi_application()
