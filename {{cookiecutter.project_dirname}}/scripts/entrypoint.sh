a547fb9d614c46d9e179146e4bb78f46f454497a
Niccolò Mineo <niccolo.mineo@gmail.com>
2021-12-23 16:23:09 +0100

    TG-348 Collect statics on container build

--- {{cookiecutter.project_dirname}}/scripts/entrypoint.sh
+++ {{cookiecutter.project_dirname}}/scripts/entrypoint.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 python3 -m wait_for_it --service "${DATABASE_URL:-"postgres:5432"}"
 python3 manage.py migrate --noinput
-python3 manage.py collectstatic --clear --noinput
 exec "$@"
