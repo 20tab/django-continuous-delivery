4352d596b0ccfc49798cbb55f35c384827871041
Niccolò Mineo <niccolo.mineo@gmail.com>
2021-12-27 20:28:12 +0100

    TG-349 Add Django deploy check

--- {{cookiecutter.project_dirname}}/scripts/check.sh
+++ {{cookiecutter.project_dirname}}/scripts/check.sh
@@ -6,5 +6,6 @@ python3 -m black --check .
 python3 -m isort --check .
 python3 -m flake8
 python3 -m mypy .
+python3 manage.py check --deploy --fail-level=WARNING --configuration=Remote
 python3 -m bandit --quiet --recursive --exclude tests .
 python3 -m safety check --file requirements/remote.txt
