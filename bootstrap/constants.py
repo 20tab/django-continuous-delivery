8e4d5068029d4bbb4a66299de151d4c856da7a36
Niccolò Mineo <niccolo.mineo@gmail.com>
2022-03-10 11:57:06 +0100

    TG-496 Refactor bootstrap

---
+++ bootstrap/constants.py
@@ -0,0 +1,7 @@
+"""Web project initialization CLI constants."""
+
+GITLAB_TOKEN_ENV_VAR = "GITLAB_PRIVATE_TOKEN"
+
+MEDIA_STORAGE_CHOICES = ["local", "s3-digitalocean", "none"]
+
+MEDIA_STORAGE_DEFAULT = "s3-digitalocean"
