b0f20a0d5ae58a0682588820a203cfc8c719bbc0
Filippo Morelli <filippo@20tab.com>
2022-07-06 18:53:56 +0000

    Fix deploy scripts

--- {{cookiecutter.project_dirname}}/scripts/deploy.sh
+++ {{cookiecutter.project_dirname}}/scripts/deploy.sh
@@ -3,10 +3,8 @@
 # init.sh must be sourced to let it export env vars
 . ${PROJECT_DIR}/scripts/deploy/init.sh
 
-sh deploy/terraform.sh validate
+sh ${PROJECT_DIR}/scripts/deploy/terraform.sh validate
 
-sh deploy/terraform.sh plan ${var_files} -input=false -out="${plan_cache}"
+sh ${PROJECT_DIR}/scripts/deploy/terraform.sh plan-json
 
-sh deploy/terraform.sh show -json "${plan_cache}" | jq -r "${JQ_PLAN}" > "${plan_json}"
-
-sh deploy/terraform.sh apply -input=false "${plan_cache}"
+sh ${PROJECT_DIR}/scripts/deploy/terraform.sh apply -auto-approve
