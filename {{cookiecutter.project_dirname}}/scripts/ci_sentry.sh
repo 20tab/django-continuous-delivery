a2c16bcc73e595fc5f7bdbbc63152ae25d13cda2
Filippo Morelli <filippo@20tab.com>
2022-07-07 23:52:33 +0000

    Fix CI

--- {{cookiecutter.project_dirname}}/scripts/ci_sentry.sh
+++ {{cookiecutter.project_dirname}}/scripts/ci_sentry.sh
@@ -6,7 +6,7 @@ if [ "${VAULT_ADDR}" != "" ]; then
   curl https://releases.hashicorp.com/vault/${VAULT_VERSION:=1.11.0}/vault_${VAULT_VERSION}_linux_386.zip --output vault.zip
   unzip vault.zip
 
-  export VAULT_TOKEN="$(./vault write -field=token auth/gitlab-jwt-${VAULT_PROJECT_PATH}/login role=gitlab-jwt-${VAULT_PROJECT_PATH}-envs-${ENV_SLUG} jwt=${CI_JOB_JWT_V2})"
+  export VAULT_TOKEN=`./vault write -field=token auth/gitlab-jwt-${VAULT_PROJECT_PATH}/login role=gitlab-jwt-${VAULT_PROJECT_PATH}-envs-${ENV_SLUG} jwt=${CI_JOB_JWT_V2})`
 
   export SENTRY_AUTH_TOKEN=`./vault kv get -format='json' -field=data ${VAULT_PROJECT_PATH}/envs/${ENV_SLUG}/sentry | jq -r .sentry_auth_token`
 
