2ccaef397570f3f2245383ea5aa19d970f13f1ff
Filippo Morelli <filippo@20tab.com>
2022-12-02 15:33:25 +0100

    Fix pacts import

--- {{cookiecutter.project_dirname}}/scripts/pact_verify.sh
+++ {{cookiecutter.project_dirname}}/scripts/pact_verify.sh
@@ -3,4 +3,4 @@
 wait-for-it --quiet --service postgres:5432 -- \
   python3 -m pytest --dc=Testing --disable-warnings \
     --pact-provider-name=$PACT_PROVIDER_NAME \
-	$@ pact/verify_pacts.py
+	$@ pacts/verify_pacts.py
