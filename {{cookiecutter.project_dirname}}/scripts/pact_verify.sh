cf3ba6011eadd601a64ae6154d3f9906c976502c
juhnicacio <juhnicacio.dev@gmail.com>
2023-01-10 17:49:40 +0100

    add ruff dependency and upgrade requirements

--- {{cookiecutter.project_dirname}}/scripts/pact_verify.sh
+++ {{cookiecutter.project_dirname}}/scripts/pact_verify.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 wait-for-it --quiet --service postgres:5432 -- \
   python3 -m pytest --dc=Testing --disable-warnings \
-    --pact-provider-name=$PACT_PROVIDER_NAME \
-	$@ pacts/verify_pacts.py
+    --pact-provider-name="$PACT_PROVIDER_NAME" \
+	"$@" pacts/verify_pacts.py
