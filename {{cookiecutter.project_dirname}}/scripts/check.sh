a787baf3fc8ee27d57633137aeace41044feff9b
juhnicacio <juhnicacio.dev@gmail.com>
2023-01-10 17:54:48 +0100

    mypy command fix

--- {{cookiecutter.project_dirname}}/scripts/check.sh
+++ {{cookiecutter.project_dirname}}/scripts/check.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 python3 -m black --check .
 python3 -m isort --check .
 python3 -m flake8
-python3 -m mypy .
+python3 -m mypy --no-site-packages .
 python3 -m bandit --quiet --recursive --exclude tests .
 python3 -m pip_audit --require-hashes --requirement requirements/remote.txt
