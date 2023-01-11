e78c79f808236dba459e369f7ef80561cca01181
juhnicacio <juhnicacio.dev@gmail.com>
2023-01-11 09:19:57 +0100

    fix after review

--- {{cookiecutter.project_dirname}}/{{cookiecutter.django_settings_dirname}}/settings.py
+++ {{cookiecutter.project_dirname}}/{{cookiecutter.django_settings_dirname}}/settings.py
@@ -93,7 +93,7 @@ class ProjectDefault(Configuration):
 
     AUTH_PASSWORD_VALIDATORS = [
         {
-            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator", # noqa: E501
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
         },
         {
             "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
