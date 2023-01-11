7545c8482c1c1a8ea30aead4b9e7fab6eaaef849
Mitchina <72406937+Mitchina@users.noreply.github.com>
2023-01-11 16:22:03 +0100

    Upgrade requirements and add ruff dependency  (#257)
    Co-authored-by: Paolo Melchiorre <paolo@melchiorre.org>
    Co-authored-by: Niccolò Mineo <niccolo@20tab.com>

--- {{cookiecutter.project_dirname}}/scripts/behave.sh
+++ {{cookiecutter.project_dirname}}/scripts/behave.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 python3 -m wait_for_it --service "${DATABASE_URL:-"postgres:5432"}"
-python3 -m manage behave --configuration=Testing --noinput --simple $@
+python3 -m manage behave --configuration=Testing --noinput --simple "$@"
