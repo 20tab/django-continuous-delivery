c655c3e9058ee4c6e3e4def280e8a98924c0bb24
Mitchina <72406937+Mitchina@users.noreply.github.com>
2023-01-11 11:23:51 +0100

    Update {{cookiecutter.project_dirname}}/scripts/check.sh
    Co-authored-by: Niccolò Mineo <niccolo@20tab.com>

--- {{cookiecutter.project_dirname}}/scripts/check.sh
+++ {{cookiecutter.project_dirname}}/scripts/check.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 python3 -m manage check
 python3 -m manage makemigrations --dry-run --check
 python3 -m black --check .
-ruff .
+python3 -m ruff .
 python3 -m bandit --quiet --recursive --exclude tests .
 python3 -m pip_audit --require-hashes --requirement requirements/remote.txt
