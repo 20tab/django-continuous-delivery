2cb090fa9c6a0842295f3bf2e230b00e855653fb
juhnicacio <juhnicacio.dev@gmail.com>
2023-01-11 10:20:09 +0100

    fixes after review

--- {{cookiecutter.project_dirname}}/scripts/check.sh
+++ {{cookiecutter.project_dirname}}/scripts/check.sh
@@ -8,4 +8,3 @@ python3 -m black --check .
 ruff .
 python3 -m bandit --quiet --recursive --exclude tests .
 python3 -m pip_audit --require-hashes --requirement requirements/remote.txt
-checkov --download-external-modules=false --evaluate-variables=true --skip-framework terraform --quiet -d .
