ea294bb7a0bdfd9d6b58e506d981f2a98db2cd9e
giulioat20tab <105198700+giulioat20tab@users.noreply.github.com>
2023-01-18 16:56:53 +0100

    Draft: TG-824 refactoring bootstrap collector (#264)
    Co-authored-by: Filippo Morelli <filippo@20tab.com>
    Co-authored-by: Juliana Nicacio <juhnicacio.dev@gmail.com>
    Co-authored-by: Matteo Vitali <matteo@20tab.com>
    Co-authored-by: Paolo Melchiorre <paolo@melchiorre.org>

--- {{cookiecutter.project_dirname}}/scripts/pact_verify.sh
+++ {{cookiecutter.project_dirname}}/scripts/pact_verify.sh
@@ -1,7 +1,8 @@
+
 #!/usr/bin/env bash
 
 set -euo pipefail
 
 python3 -m pytest --dc=Testing --disable-warnings \
-  --pact-provider-name="$(PACT_PROVIDER_NAME)" \
-  "$@" pacts/verify_pacts.py
+  --pact-provider-name="${PACT_PROVIDER_NAME}" \
+  "${@}" pacts/verify_pacts.py
