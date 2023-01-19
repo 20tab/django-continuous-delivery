8d3a3648008e322caf183ba1d02ceb6624e27c6e
Matteo Vitali <matteo@20tab.com>
2023-01-19 16:55:30 +0100

    Add django check deploy ci cd job

--- {{cookiecutter.project_dirname}}/scripts/trivy.sh
+++ {{cookiecutter.project_dirname}}/scripts/trivy.sh
@@ -7,7 +7,7 @@ time trivy image --download-db-only --no-progress
 time trivy image \
     --exit-code 0 \
     --format template \
-    --output "/tmp/report.html" \
+    --output "/trivy/report.html" \
     --security-checks vuln \
     --template "@contrib/html.tpl" \
     ${TARGET_IMAGE}
