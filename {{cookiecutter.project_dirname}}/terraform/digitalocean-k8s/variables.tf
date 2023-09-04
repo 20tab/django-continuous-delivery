a3bd5ed527d4eff9aac92ac7c9a0be0c2d7048db
Daniele Pompa <daniele.pompa@20tab.com>
2023-09-04 13:12:57 +0200

    Fix missing variable

--- {{cookiecutter.project_dirname}}/terraform/digitalocean-k8s/variables.tf
+++ {{cookiecutter.project_dirname}}/terraform/digitalocean-k8s/variables.tf
@@ -1,3 +1,10 @@
+variable "cache_url" {
+  type        = string
+  description = "A Django cache URL override."
+  default     = ""
+  sensitive   = true
+}
+
 variable "digitalocean_token" {
   description = "The Digital Ocean access token."
   type        = string
